9577f12dd8699587a5562bd8fedea911a2075ef6
Aharon ulano <113341585+aharonulano@users.noreply.github.com>
2024-12-03 02:55:42 +0200

    add an igw and nat-gw and route tables and EC2s

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "${var.aws_region}-a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}-a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -17,11 +17,89 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.2.0/4"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "${var.aws_region}-b"
 
   tags = {
     Name = "private-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "main-nat-gw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    carrier_gateway_id = "0.0.0.0/0"
+    gateway_id         = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_instance" "public_instance" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "public-instance"
+  }
+}
+
+
+resource "aws_instance" "private_instance" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private.id
+
+  tags = {
+    Name = "private-instance"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,35 @@
 variable "account" {
   description = "(Required) the account your working on"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "profile_name" {
   description = "(Required) the profile you are using to work on aws different users different permissions and tasks"
-  type = string
-  default = "a-user-name"
+  type        = string
+  default     = "a-user-name"
 }
 
 variable "aws_region" {
   description = "(Required) which region are you going to work on"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr_block" {
   description = "(Required) the range of the IPs for your Virtual Private Cloud"
-  type = string
-  default = "10.0.0.0/16"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
 }
